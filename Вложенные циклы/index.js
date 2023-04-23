9268c69eab4e58dd48cc3074f5e0b0b6564e5ec6
Endakota <endakota09@gmail.com>
2023-04-23 13:07:23 +0300

    Вложенные циклы

---
+++ Вложенные циклы/index.js
@@ -0,0 +1,46 @@
+/*
+1
+22
+333
+4444
+*/ 
+let p = document.querySelector("#text")
+// for(let i = 1; i <= 5; i++){
+//     p.innerText += "["
+//     for(let j = 0; j < i; j++){
+//         p.innerText += " "+ i
+//     }
+//     p.innerText += "] \n"
+// }
+/*
+Нужно заполнить массив нулями
+*/
+// var zeros = []
+// var n = 5
+
+// for(let i = 0; i < n; i++){
+//     zeros.push(0)
+// }
+// p.innerText = zeros
+
+/* 
+Нужно заполнить двумерный массив нулями n * n
+*/
+
+// var zeros = []
+// var n = 5
+// for(let i = 0; i < n; i++){
+//     zeros.push([])
+//     for(let j = 0; j < n; j++){
+//         zeros[i].push(0)
+//     }
+// }
+// console.log(zeros)
+
+// i = 0 -> zeros[0] -> [[]]
+// i = 1 -> [[],[]] -> [[],[0]]
+// i = 2 -> [[],[0], []] -> [[],[0],[0,0]]
+
+
+
+
